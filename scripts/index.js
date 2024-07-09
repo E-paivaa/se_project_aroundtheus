794edae535c181c6c4ce25a187f448a37d1144e6
E-paivaa <United_hitz-1@hotmail.com>
2024-07-08 21:14:35 -0400

    Project 6 -index.js corrections

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-  },    
+  },
   {
     name: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
@@ -11,7 +11,7 @@ const initialCards = [
     name: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
-  { 
+  {
     name: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
@@ -22,11 +22,10 @@ const initialCards = [
   {
     name: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-  }    
+  },
 ];
 
 // Elements
-
 
 const profileName = document.querySelector(".profile__name");
 const profileTitle = document.querySelector(".profile__title");
@@ -49,33 +48,34 @@ const previewImageModal = document.querySelector("#preview-image-modal");
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditCloseButton = profileEditModal.querySelector(
-  "#profile-edit-close-button"
+  "#profile-edit-close-button",
 );
-const addCardCloseButton = document.querySelector("#add-card-modal-button-close");
+const addCardCloseButton = document.querySelector(
+  "#add-card-modal-button-close",
+);
 const previewImageCloseButton = document.querySelector(
-  "#preview-image-close-button"
+  "#preview-image-close-button",
 );
-
 
 function openModal(modal) {
-  if(modal){
-  modal.classList.add("modal_opened");
-}
+  if (modal) {
+    modal.classList.add("modal_opened");
+  }
   document.addEventListener("keydown", closeModalESC);
 }
 
 function closeModal(modal) {
-  if(modal){
-  modal.classList.remove("modal_opened");
+  if (modal) {
+    modal.classList.remove("modal_opened");
   }
   document.removeEventListener("keydown", closeModalESC);
 }
 
 function closeModalESC(evt) {
   if (evt.key === "Escape") {
-  const openModals = document.querySelectorAll(".modal_opened");
-  openModals.forEach(closeModal);
-}
+    const openedModal = document.querySelector(".modal_opened");
+    closeModal(openedModal);
+  }
 }
 
 function renderCard(cardData, cardListEl) {
@@ -110,12 +110,13 @@ function getCardElement(cardData) {
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
   const deleteButton = cardElement.querySelector(".card__delete-button");
-  const likeButton = cardElement.querySelector(".card__like-button")
+  const likeButton = cardElement.querySelector(".card__like-button");
 
-  
   cardImageEl.addEventListener("click", () => {
     const modalImage = previewImageModal.querySelector("#modal-image");
-    const modalTitle = previewImageModal.querySelector("#modal-preview-image-title");
+    const modalTitle = previewImageModal.querySelector(
+      "#modal-preview-image-title",
+    );
     modalImage.src = cardData.link;
     modalImage.alt = cardData.name;
     modalTitle.textContent = cardData.name;
@@ -151,16 +152,20 @@ profileEditCloseButton.addEventListener("click", () => {
   closeModal(profileEditModal);
 });
 
-addCardCloseButton.addEventListener("click", () => { 
-  closeModal(addCardModal);});
+addCardCloseButton.addEventListener("click", () => {
+  closeModal(addCardModal);
+});
 
 const modals = document.querySelectorAll(".modal");
 modals.forEach((modal) => {
   modal.addEventListener("mousedown", (evt) => {
     if (evt.target.classList.contains("modal")) {
-      closePopup(modal);
+      closeModal(modal);
     }
   });
 });
 
-initialCards.forEach((cardData) => renderCard(cardData, cardListEl));
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
+});
